d535265b10eb4fc6e17bcb05c6bf740226d2b5eb
Ankura <Master-Anku.com>
2024-05-01 06:26:43 +0630

    true and flase CND

---
+++ script.js
@@ -0,0 +1,25 @@
+// TODO: Change the values and operators below to test your algorithm meets all conditions
+var x = 50;
+var expression1 = x < 60;
+var expression2 = x > 50;
+
+switch (true) {
+    case x == 50:
+        console.log('True and True')
+         break;
+    case x < 45:
+        console.log('true and flase');
+        break;
+    case x > 60:
+        console.log('flase and true');
+        break;
+    default:
+        console.log('flase and flase')
+}
+
+// TODO: Write Your JavaScript Code Here
+
+//var x = prompt("Number");
+
+
+
